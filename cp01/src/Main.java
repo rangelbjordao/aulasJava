48f40c236bf12bf4aaa0ea8aa940147901db9f30
rangelbjordao <96200914+rangelbjordao@users.noreply.github.com>
2024-09-26 09:02:57 -0300

    cp 01

---
+++ cp01/src/Main.java
@@ -0,0 +1,17 @@
+import entidades.EpisodioPodcast;
+import entidades.Musica;
+import entidades.Playlist;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        var musica1 = new Musica("one", 180, "Metallica", "Black Album", "Metal");
+
+        var ep1 = new EpisodioPodcast("OO com Java", 3600, "PodJava", "JavaMan", "Neeste episodio falamos osbre OO com Java");
+
+        var playlist = new Playlist(new ArrayList<>(), "Meus favoritos");
+        playlist.adicionarMidia(musica1);
+        playlist.adicionarMidia(ep1);
+    }
+}
---
+++ cp01/src/entidades/EpisodioPodcast.java
@@ -0,0 +1,78 @@
+package entidades;
+
+import java.util.Objects;
+
+public class EpisodioPodcast extends Midia {
+    // Atributos
+    private String nome;
+    private String apresentador;
+    private String descricao;
+
+    // Metodos
+    @Override
+    public void reproduzir() {
+        System.out.println("Reproduzindo episodio podcast:" + this.getTitulo());
+    }
+
+    // Construtor vazio
+    public EpisodioPodcast() {
+    }
+
+    //Construtor completo
+    public EpisodioPodcast(String titulo, int duracao, String nome, String apresentador, String descricao) {
+        super(titulo, duracao);
+        this.nome = nome;
+        this.apresentador = apresentador;
+        this.descricao = descricao;
+    }
+
+    // getters e setter
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getApresentador() {
+        return apresentador;
+    }
+
+    public void setApresentador(String apresentador) {
+        this.apresentador = apresentador;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    // equals e hashcode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        EpisodioPodcast that = (EpisodioPodcast) o;
+        return Objects.equals(getNome(), that.getNome()) && Objects.equals(getApresentador(), that.getApresentador()) && Objects.equals(getDescricao(), that.getDescricao());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getNome(), getApresentador(), getDescricao());
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "EpsodioPodcast{" +
+                "nome='" + nome + '\'' +
+                ", apresentador='" + apresentador + '\'' +
+                ", descricao='" + descricao + '\'' +
+                "} " + super.toString();
+    }
+}
