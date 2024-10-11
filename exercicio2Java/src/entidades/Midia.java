a5a7e2e1686e270f2e379f4238ba6a1c56cf4205
rangelbjordao <96200914+rangelbjordao@users.noreply.github.com>
2024-10-11 14:39:38 -0300

    Add files via upload

---
+++ exercicio2Java/src/entidades/Midia.java
@@ -0,0 +1,71 @@
+package entidades;
+
+import java.util.Objects;
+
+public abstract class Midia {
+    // Atributos
+    private String titulo;
+    private int duracao;
+
+
+    // Metodos
+    public void reproduzir() {
+        System.out.println("Reproduzir midia:" + this.titulo);
+    }
+
+    // Contrutor vazio
+    public Midia() {
+    }
+
+    // Construtor completo
+    public Midia(String titulo, int duracao) {
+        this.titulo = titulo;
+        this.duracao = duracao;
+    }
+
+    // Getters e Setters
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public void setDuracao(int duracao) {
+        this.duracao = duracao;
+    }
+
+    // equals e hashcode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Midia midia = (Midia) o;
+        return getDuracao() == midia.getDuracao() && Objects.equals(getTitulo(), midia.getTitulo());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitulo(), getDuracao());
+    }
+
+    //toString
+    @Override
+    public String toString() {
+        return "Midia{" +
+                "titulo='" + titulo + '\'' +
+                ", duracao=" + duracao +
+                '}';
+    }
+
+    public String exibirDetalhes() {
+        return "Titulo: " + this.titulo + "\n"
+                + "Duracao: " + this.duracao + "\n"
+                + "---" + "\n";
+    }
+}
